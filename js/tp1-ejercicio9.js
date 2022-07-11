5ed9cb4176e3798e007a62e92d163c804ec15b79
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-07-11 19:21:54 -0300

    agregué opción de ejercicio 9 del tp1 utilizando estructura for

--- js/tp1-ejercicio9.js
+++ js/tp1-ejercicio9.js
@@ -14,3 +14,12 @@ while (i < frase.length) {
         i++
     }
 }
+
+// for(let i = 0 ; i < frase.length ; i++){
+//     if (frase.charAt(i) === "a" || frase.charAt(i) === "á" || frase.charAt(i) === "e" || 
+//     frase.charAt(i) === "é" || frase.charAt(i) === "i" || frase.charAt(i) === "í" || 
+//     frase.charAt(i) === "o" || frase.charAt(i) === "ó" || frase.charAt(i) === "u" || 
+//     frase.charAt(i) === "ú") {
+//         document.write(frase.charAt(i))
+//     }
+// }
