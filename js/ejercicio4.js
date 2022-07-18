f2330a207e3baa9da70909fd8d83d7ec729a5e59
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-07-18 11:44:15 -0300

    realicé el ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,17 @@
+let resultado = 0
+let indice = 1
+
+while(confirm("¿ingresar número?") === true){
+    if (confirm) {
+        numero = parseInt(prompt("ingrese número"));
+        if(numero >= 0){
+            resultado = resultado + numero
+            document.write(indice + "° número ingresado: " + numero);
+            document.write("<br>");
+            indice++
+        } else {
+            alert("el carácter ingresado no es un número");
+        }
+    }
+}
+document.write("Resultado de la suma: " + resultado);
