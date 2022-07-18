61a562a42991ce77b00814090a9388997c5be6b3
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-07-18 13:59:22 -0300

    quité un condicional if duplicado en el ejercicio 4

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -2,16 +2,14 @@ let resultado = 0
 let indice = 1
 
 while(confirm("¿ingresar número?") === true){
-    if (confirm) {
-        numero = parseInt(prompt("ingrese número"));
-        if(numero >= 0){
-            resultado = resultado + numero
-            document.write(indice + "° número ingresado: " + numero);
-            document.write("<br>");
-            indice++
-        } else {
-            alert("el carácter ingresado no es un número");
-        }
+    numero = parseInt(prompt("ingrese número"));
+    if(numero >= 0){
+        resultado = resultado + numero
+        document.write(indice + "° número ingresado: " + numero);
+        document.write("<br>");
+        indice++
+    } else {
+        alert("el carácter ingresado no es un número");
     }
 }
 document.write("Resultado de la suma: " + resultado);
